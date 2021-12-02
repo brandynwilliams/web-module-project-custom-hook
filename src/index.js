2d2517eac6109b688ad4e4dc6cbe60b53fdbc1ff
Brandyn <stateofw@gmail.com>
2021-12-01 20:46:45 -0500

    mvp completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+import Charts from "./components/Charts";
+import Navbar from "./components/Navbar";
+import { useDarkMode } from "./hooks/useDarkMode";
+
+import "./style.css";
+
+const App = () => {
+  const [coinData, setCoinData] = useState([]);
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then(res => setCoinData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div className={darkMode ? "dark-mode App" : "App"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Charts coinData={coinData} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
